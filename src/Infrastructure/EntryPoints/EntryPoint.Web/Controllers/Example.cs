ba63dc4678e254de9431deabbd131134fb513951
Juan Daniel <jdarce91@misena.edu.co>
2023-09-20 19:17:42 -0500

    initial template

---
+++ src/Infrastructure/EntryPoints/EntryPoint.Web/Controllers/Example.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace $safeprojectname$.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class Example : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
